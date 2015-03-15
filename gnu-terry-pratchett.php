3efbd3d655421bfc6f0dca6736d8401ab14cd8d0
Nick Cernis <nick@cern.is>
2015-03-15 21:35:51 +0000

    Bump version

--- gnu-terry-pratchett.php
+++ gnu-terry-pratchett.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: GNU Terry Pratchett
- * Version: 0.1.1
+ * Version: 0.1.2
  * Plugin URI: https://github.com/nickcernis/gnu-terry-pratchett
  * Description: Adds an X-Clacks-Overhead HTTP header set to 'GNU Terry Pratchett' on all non-admin pages.
  * Author: Nick Cernis
